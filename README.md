30e04eb9a3b9e228c22d483445731e4f0ce7a505
Leonardo Lewandowski <leonardo.lewandowski@mercadolibre.com>
2019-03-18 21:44:15 -0300

    Add shields

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# carpenter
+# Carpenter [![npm version](https://img.shields.io/npm/v/carpenter.svg)](https://www.npmjs.com/package/carpenter) [![license type](https://img.shields.io/npm/l/carpenter.svg)](https://github.com/llewan/carpenter.git/blob/master/LICENSE) [![codecov](https://codecov.io/gh/llewan/carpenter/branch/develop/graph/badge.svg)](https://codecov.io/gh/llewan/carpenter)
 Creates a builder pattern for NodeJS using ES6 proxy.
+
+## Installation
+```
+npm install --save carpenter
+```
+
+And then import it:
+```
+// ES6 modules
+import { Builder, BuilderType } from 'rex-react';
+
+// commonjs
+const Builder = require('carpenter').Builder;
+const BuilderType = require('carpenter').BuilderType;
+```
+
+## Usage
+Specify a template object which allows to define the API and the dataType for each field.
+```
+const template = {
+  id: BuilderType.number,
+  first_name: BuilderType.string,
+  email: BuilderType.string,
+};
+
+const built = Builder(template)
+      .id(1)
+      .firstName('Leo')
+      .email('leolewan@gmail.com')
+      .build();
+```
+
+###Note
+- It accepts the build function on camelCase as well
+
+
+## Questions or suggestions?
+Feel free to contact me on [Twitter](https://twitter.com/leolewan) or [open an issue](https://github.com/llewan/carpenter/issues/new).
--- index.spec.js
+++ index.spec.js
@@ -5,18 +5,18 @@ const { Builder, BuilderType } = require('./index');
 describe('Builder', () => {
   it('should properly chain methods - same data type', () => {
     const template = {
-      name: BuilderType.string,
-      surname: BuilderType.string,
+      first_name: BuilderType.string,
+      email: BuilderType.string,
     };
 
     const built = Builder(template)
-      .name('Leo')
-      .surname('Lewan')
+      .firstName('Leo')
+      .email('leolewan@gmail.com')
       .build();
 
     const expected = {
-      name: 'Leo',
-      surname: 'Lewan',
+      first_name: 'Leo',
+      email: 'leolewan@gmail.com',
     };
 
     expect(built).to.deep.equals(expected);
@@ -25,26 +25,20 @@ describe('Builder', () => {
   it('should properly chain methods - different data types', () => {
     const template = {
       name: BuilderType.string,
-      surname: BuilderType.string,
       address: BuilderType.object,
       age: BuilderType.number,
-      start_school_at: BuilderType.number,
     };
 
     const built = Builder(template)
       .name('Leo')
-      .surname('Lewan')
       .address({})
       .age(30)
-      .startSchoolAt(5)
       .build();
 
     const expected = {
       name: 'Leo',
-      surname: 'Lewan',
       address: {},
       age: 30,
-      start_school_at: 5
     };
 
     expect(built).to.deep.equals(expected);
@@ -70,36 +64,32 @@ describe('Builder', () => {
       .surname([])
       .build();
 
-    assert.throws(built, 'surname expects a string data type and array is being received')
+    assert.throws(built, 'Invalid error `surname` of type `array` supplied to Builder, expected `string`.')
   });
 
   it('should throw an error when calling a function with the wrong primitive data type - case 2', () => {
     const template = {
       name: BuilderType.string,
-      surname: BuilderType.string,
       address: BuilderType.array,
     };
     const built = () => Builder(template)
       .name('Leo')
-      .surname('Lewan')
-      .address('qqq')
+      .address('aaa')
       .build();
 
-    assert.throws(built, 'address expects a array data type and string is being receive')
+    assert.throws(built, 'Invalid error `address` of type `string` supplied to Builder, expected `array`.')
   });
 
   it('should throw an error when calling a function with the wrong primitive data type - case 3', () => {
     const template = {
       name: BuilderType.string,
-      surname: BuilderType.string,
       address: BuilderType.object,
     };
     const built = () => Builder(template)
     .name('Leo')
-    .surname('Lewan')
     .address([])
     .build();
 
-    assert.throws(built, 'address expects a object data type and array is being received')
+    assert.throws(built, 'Invalid error `address` of type `array` supplied to Builder, expected `object`.')
   });
 });
